51bcc6c9bd6fa0e5c964ed81f78f25a62ca5af1a
tmori <tmori@esm.co.jp>
2021-02-20 17:43:02 +0900

    add server

---
+++ impl/asset/server/csharp/HakoniwaServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using HakoniwaGrpc;
+
+namespace HakoniwaService
+{
+    class HakoniwaServer : HakoniwaCoreService.HakoniwaCoreServiceBase
+    {
+        private static Server server;
+        static public void StartServer(string ipaddr, int portno)
+        {
+            if (HakoniwaServer.server != null)
+            {
+                throw new InvalidOperationException();
+            }
+            HakoniwaServer.server = new Server
+            {
+                Services = { HakoniwaCoreService.BindService(new HakoniwaServer()) },
+                Ports = { new ServerPort(ipaddr, portno, ServerCredentials.Insecure) }
+            };
+            server.Start();
+        }
+        static public void ShutdownServer()
+        {
+            HakoniwaServer.server.ShutdownAsync().Wait();
+            HakoniwaServer.server = null;
+        }
+        public override Task<HakoniwaReply> Register(HakoniwaAssetInfo request, ServerCallContext context)
+        {
+            Console.WriteLine("Register");
+            return Task.FromResult(new HakoniwaReply
+            {
+                Ercd = "OK"
+            });
+        }
+        public override Task<HakoniwaReply> Unregister(HakoniwaAssetInfo request, ServerCallContext context)
+        {
+            Console.WriteLine("Unregister");
+            return Task.FromResult(new HakoniwaReply
+            {
+                Ercd = "OK"
+            });
+        }
+    }
+
+
+}
---
+++ impl/asset/server/csharp/MainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HakoniwaService
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string ipaddr = "172.25.144.1";
+            int portno = 50051;
+
+            Console.WriteLine("ipaddr=" + ipaddr + " portno=" + portno.ToString());
+            Console.WriteLine("Hello World!!");
+            HakoniwaServer.StartServer(ipaddr, portno);
+
+
+            Console.WriteLine("Press any key to shutdown the server...");
+            Console.ReadKey();
+
+            HakoniwaServer.ShutdownServer();
+
+        }
+    }
+}

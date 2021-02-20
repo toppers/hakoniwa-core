71ffa418e82f52cc9dac2701ebf615832f59e122
root <root@MyComputer.localdomain>
2021-02-20 06:26:04 +0000

    add client build

--- docker/run.bash
+++ docker/run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ATHRILL_TOP_DIR=$(cd ../../.. && pwd)
+HAKONIWA_TOP_DIR=$(cd .. && pwd)
 DOCKER_IMAGE=toppers/hakoniwa-core:v1.0.0
 
-sudo docker run \
+sudo docker run -v ${HAKONIWA_TOP_DIR}:/root/workspace/hakoniwa \
 		-it --rm --net host --name hakoniwa-core ${DOCKER_IMAGE} 

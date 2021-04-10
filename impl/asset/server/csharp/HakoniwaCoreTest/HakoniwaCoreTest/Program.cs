4356991288fde2f7ee21a235eb098c75075b0b51
Takashi Mori <tmori@esm.co.jp>
2021-04-10 10:17:59 +0900

    add hakoniwa core test

--- impl/asset/server/csharp/HakoniwaCoreTest/HakoniwaCoreTest/Program.cs
+++ impl/asset/server/csharp/HakoniwaCoreTest/HakoniwaCoreTest/Program.cs
@@ -10,39 +10,18 @@ namespace HakoniwaCoreTest
     {
         static void Main(string[] args)
         {
-            string filepath = @"C:\project\oss\hakoniwa-core\impl\asset\server\csharp\HakoniwaCoreTest\HakoniwaCoreTest\core_config.json";
+            string filepath = @"..\..\..\core_config.json";
             AssetConfigLoader.Load(filepath);
-#if false
-            string ipaddr = "172.18.144.1";
-            int portno = 50051;
+            string ipaddr = AssetConfigLoader.core_config.core_ipaddr;
+            int portno = AssetConfigLoader.core_config.core_portno;
 
             Console.WriteLine("ipaddr=" + ipaddr + " portno=" + portno.ToString());
             RpcServer.StartServer(ipaddr, portno);
-
-            LineBinaryStorageTest test = new LineBinaryStorageTest();
-            test.DoTest();
-
-
-            Console.WriteLine("Press any key to Start...");
-            Console.ReadKey();
-
-            SimulationController simulator = RpcServer.GetSimulator();
-            simulator.Start();
-
-            Console.WriteLine("Press any key to End...");
-            Console.ReadKey();
-            simulator.Stop();
-
-
-            Console.WriteLine("Press any key to Terminate...");
-            Console.ReadKey();
-            simulator.Terminate();
 
             Console.WriteLine("Press any key to shutdown the server...");
             Console.ReadKey();
 
             RpcServer.ShutdownServer();
-#endif
         }
     }
 }

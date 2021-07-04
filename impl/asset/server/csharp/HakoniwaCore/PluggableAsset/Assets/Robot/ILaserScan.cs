362c25a3934373019bc88b219f2604bd4d2b5973
tmori <tmori@esm.co.jp>
2021-07-04 10:59:34 +0900

    add TB3 Model

---
+++ impl/asset/server/csharp/HakoniwaCore/PluggableAsset/Assets/Robot/ILaserScan.cs
@@ -0,0 +1,13 @@
+﻿using Hakoniwa.PluggableAsset.Communication.Pdu;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hakoniwa.PluggableAsset.Assets.Robot
+{
+    public interface ILaserScan : IRobotSensor
+    {
+        void UpdateSensorData(Pdu pdu);
+    }
+}
+
---
+++ impl/asset/server/csharp/HakoniwaCore/PluggableAsset/Assets/Robot/TB3/ITB3Parts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hakoniwa.PluggableAsset.Assets.Robot.TB3
+{
+    public interface ITB3Parts
+    {
+        string GetMotor(int index);
+        string GetIMU();
+        string GetLaserScan();
+    }
+}

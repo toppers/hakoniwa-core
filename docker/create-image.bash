f14eeeb50bfd35100fa08165a72b3a63f1f60f47
Takashi Mori <tmori@esm.co.jp>
2021-08-05 06:01:43 +0900

    fix #19

--- docker/create-image.bash
+++ docker/create-image.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_IMAGE=toppers/hakoniwa-core
+DOCKER_IMAGE=toppers/hakoniwa-client-builder
 DOCKER_TAG=v1.0.0
 sudo docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} -f Dockerfile .
 

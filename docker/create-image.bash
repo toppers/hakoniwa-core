8789200cf7ab208deb307e5ded9807bb15d7e651
Takashi Mori <tmori@esm.co.jp>
2021-08-05 15:16:59 +0900

    fix #20

--- docker/create-image.bash
+++ docker/create-image.bash
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-DOCKER_IMAGE=toppers/hakoniwa-client-builder
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <ubuntu version>"
+	exit 1
+fi
+
+VERSION=$1
+if [ $VERSION = "18" ]
+then
+	DOCKER_IMAGE=toppers/hakoniwa-client-builder-18
+	DOCKER_FILE=Dockerfile.ubuntu18
+else
+	DOCKER_IMAGE=toppers/hakoniwa-client-builder
+	DOCKER_FILE=Dockerfile
+fi
 DOCKER_TAG=v1.0.0
-sudo docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} -f Dockerfile .
+sudo docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} -f ${DOCKER_FILE} .
 
--- docker/run.bash
+++ docker/run.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <ubuntu version>"
+	exit 1
+fi
+VERSION=$1
+if [ $VERSION = "18" ]
+then
+	DOCKER_IMAGE=toppers/hakoniwa-client-builder-18
+else
+	DOCKER_IMAGE=toppers/hakoniwa-client-builder
+fi
+
 HAKONIWA_TOP_DIR=$(cd .. && pwd)
-DOCKER_IMAGE=toppers/hakoniwa-client-builder:v1.0.0
+DOCKER_IMAGE=${DOCKER_IMAGE}:v1.0.0
 
 sudo docker ps > /dev/null
 if [ $? -ne 0 ]

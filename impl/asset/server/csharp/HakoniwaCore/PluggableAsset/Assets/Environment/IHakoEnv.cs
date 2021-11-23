320b99f218d0dd01c162a757cf19f5726e5950bd
Takashi Mori <kanetugu2015@gmail.com>
2021-11-23 17:09:07 +0900

    add env

---
+++ impl/asset/server/csharp/HakoniwaCore/PluggableAsset/Assets/Environment/IHakoEnv.cs
@@ -0,0 +1,12 @@
+﻿using Hakoniwa.PluggableAsset.Communication.Pdu;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hakoniwa.PluggableAsset.Assets.Environment
+{
+    public interface IHakoEnv
+    {
+        void Initialize(System.Object root);
+    }
+}
---
+++ impl/asset/server/csharp/HakoniwaCore/PluggableAsset/Assets/Environment/IHakoEnvObstacle.cs
@@ -0,0 +1,13 @@
+﻿using Hakoniwa.PluggableAsset.Communication.Pdu;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hakoniwa.PluggableAsset.Assets.Environment
+{
+    public interface IHakoEnvObstacle : IHakoEnv
+    {
+        bool IsTouched();
+    }
+}
+
